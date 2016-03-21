3cfcb1e8ca98a5cf11f287f99180767e960078f4
Krystan Menne and Patrick Sullivan <kmenne@gmail.com>
2016-03-21 11:45:48 -0700

    test for failed login

--- spec/features/user_authentication_pages_spec.rb
+++ spec/features/user_authentication_pages_spec.rb
@@ -1,10 +1,19 @@
 require 'rails_helper'
 
 describe 'The user authentication path' do
+  before :each do
+    @user = create(:user)
+  end
+
   it 'allows a user to log out' do
-    user = create(:user)
-    login(user)
+    login(@user)
     click_link 'Sign Out'
     expect(page).to have_content('signed out')
+  end
+
+  it 'doesnt allow a user to login with improper credentials' do
+    incorrect_credentials = build(:user, password: 'wrong password')
+    login(incorrect_credentials)
+    expect(page).to have_content('invalid')
   end
 end
